5e87c69652589f3ad81cfd74b088f7122541ff6a
Brian Alvarez <brian.alvarez@southworks.com>
2023-10-27 13:46:21 -0300

    format main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -10,7 +10,7 @@ resource "azurerm_windows_web_app" "appservicename" {
   name                = "test-test-test"
   location            = azurerm_resource_group.rgtestname.location
   resource_group_name = azurerm_resource_group.rgtestname.name
-  service_plan_id      = azurerm_service_plan.splanname.id
+  service_plan_id     = azurerm_service_plan.splanname.id
 
   site_config {}
 }
