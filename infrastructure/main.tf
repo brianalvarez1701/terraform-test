8f839e44091de2711dda79fba5310334f59c320a
Brian Alvarez <brian.alvarez@southworks.com>
2023-10-27 13:45:32 -0300

    changes in main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,4 +1,16 @@
-resource "azurerm_resource_group" "rgtestname" {
-  name     = var.resource_group_name
-  location = var.location
+resource "azurerm_service_plan" "splanname" {
+  name                = "test-test-test"
+  resource_group_name = azurerm_resource_group.rgtestname.name
+  location            = azurerm_resource_group.rgtestname.location
+  os_type             = "Windows"
+  sku_name            = "P1v2"
+}
+
+resource "azurerm_windows_web_app" "appservicename" {
+  name                = "test-test-test"
+  location            = azurerm_resource_group.rgtestname.location
+  resource_group_name = azurerm_resource_group.rgtestname.name
+  service_plan_id      = azurerm_service_plan.splanname.id
+
+  site_config {}
 }
