4d43479b03181736f39fd3b874da2a09a7abda48
Brian Alvarez <brian.alvarez@southworks.com>
2023-10-27 13:50:41 -0300

    changing vars

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,15 +1,15 @@
 resource "azurerm_service_plan" "splanname" {
   name                = "test-test-test"
-  resource_group_name = azurerm_resource_group.rgtestname.name
-  location            = azurerm_resource_group.rgtestname.location
+  location            = var.location
+  resource_group_name = var.resource_group_name
   os_type             = "Windows"
   sku_name            = "P1v2"
 }
 
 resource "azurerm_windows_web_app" "appservicename" {
   name                = "test-test-test"
-  location            = azurerm_resource_group.rgtestname.location
-  resource_group_name = azurerm_resource_group.rgtestname.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
   service_plan_id     = azurerm_service_plan.splanname.id
 
   site_config {}
