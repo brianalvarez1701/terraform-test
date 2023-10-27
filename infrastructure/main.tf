14861fd372dcd7eb4db9d31ec2f0f962d9228629
Brian Alvarez <brian.alvarez@southworks.com>
2023-10-27 13:15:57 -0300

    main.tf change

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -15,15 +15,3 @@ resource "azurerm_resource_group" "rgtestname" {
   name     = var.resource_group_name
   location = var.location
 }
-
-resource  "azuread_application"  "app" {
-    display_name  =  "GitHub Actions"
-}
- 
-resource  "azuread_service_principal"  "app" {
-    application_id  =  azuread_application.app.client_id
-}
-
-resource  "azuread_application_password"  "example" {
-    application_object_id  =  azuread_application.app.object_id
-}
