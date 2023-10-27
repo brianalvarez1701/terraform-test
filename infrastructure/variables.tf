e0c89a06c6794dd578fc7d2785b8252dc67a4cad
Brian Alvarez <brian.alvarez@southworks.com>
2023-10-27 13:37:23 -0300

    changes in vars and tf

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,8 +1,9 @@
-variable "resource_group_name" {
-    type = string
-    default = "rg-brian"
-}
 variable "location" {
-    type = string
-    default = "eastus"
+  type    = string
+  default = "eastus"
+}
+
+variable "resource_group_name" {
+  type    = string
+  default = "rg-brian"
 }
